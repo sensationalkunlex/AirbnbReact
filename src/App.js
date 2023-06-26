d2f9a27b7a2697378f6b564f575ce67e4af83d4b
Oyekunle Oyewusi <sensationalkunlex@gmail.com>
2023-06-26 01:29:50 +0100

    modify the top menu

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 import Nav from './pages/home/TopNav/TopNav';
 import icon from './assets/icons/profile.png'
  
--- src/pages/home/TopNav/TopNav.jsx
+++ src/pages/home/TopNav/TopNav.jsx
@@ -7,24 +7,24 @@ import './TopNav.css'
 export default function TopNav(props){
 
 return (
-<Navbar collapseOnSelect expand="lg" className="bg-body-white nav-container">
-  <Container className="d-flex justify-content-between">
-    <Navbar.Brand href="#home"><img src={logo}/> <span className="text-danger"> Airbnb</span> </Navbar.Brand>
+<Navbar collapseOnSelect expand="lg" className="bg-body-white nav-container align-items-baseline">
+  
+    <Navbar.Brand href="#home"><img src={logo}/> </Navbar.Brand>
     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
     <Navbar.Collapse id="responsive-navbar-nav ">
       <Nav className="mx-auto">
-        <Nav.Link href="#stay" className="active">Stay</Nav.Link>
-        <Nav.Link href="#exprience">Experience</Nav.Link>
-        <Nav.Link href="#online-exprience">Online Experience</Nav.Link>
+        <Nav.Link href="#stay" className="active">Stays</Nav.Link>
+        <Nav.Link href="#exprience">Experiences</Nav.Link>
+        <Nav.Link href="#online-exprience">Online Experiences</Nav.Link>
       </Nav>
       <Nav>
       
-        <Nav.Link eventKey={2} href="#memes">
+        <Nav.Link eventKey={2} href="#memes" className="p-0 pt-1">
         <img src={props.icon} className='profile__img'/>
         </Nav.Link>
       </Nav>
     </Navbar.Collapse>
-  </Container>
+  {/* </Container> */}
 </Navbar>
 )
 
