72ce536631486b65894f439e9f6245f0a3a9e841
Oyekunle Oyewusi <sensationalkunlex@gmail.com>
2023-06-27 22:54:56 +0100

    slider-updated

--- src/pages/home/MiddleNav/MiddleNav.jsx
+++ src/pages/home/MiddleNav/MiddleNav.jsx
@@ -1,49 +1,80 @@
 import "./MiddleNav.css";
 import logo1 from "../../../assets/svg/logo1.svg";
 import logo2 from "../../../assets/svg/logo2.svg";
+import filter from "../../../assets/icons/filter.png";
 import calandar from "../../../assets/svg/calandar.svg";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { useState } from "react";
+import Filter from "./components/Filter/Filter";
 
 export default function MiddleNav() {
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(null);
+  const [guest, setGuest] = useState(1);
+  const [filterCheck, setFilterCheck] = useState(false);
 
   const onChange = (dates) => {
     const [start, end] = dates;
     setStartDate(start);
     setEndDate(end);
   };
-  return (
-    <div className="framer">
-      <div className="android d-flex gx-3 align-items-center">
-        <img src={logo1} className="first-icon" />
-        <img src={logo2} className="second-icon" />
-      </div>
-      <div className="middle-filter d-flex gx-3 align-items-center">
-        <select name="" id="" className="select me-3">
-          <option value="" selected={true}>
-            Anywhere
-          </option>
-        </select>
-        <div className="d-flex">
-          <div className="d-flex date-icon align-items-center">
-            <img src={calandar} alt="" className="calendar" />
-          </div>
+  function filterAction() {
+    setFilterCheck((x) => !x);
+  }
 
-          <DatePicker
-            className="me-3"
-            selected={startDate}
-            onChange={onChange}
-            startDate={startDate}
-            endDate={endDate}
-            selectsRange
-            dateFormat="MMM dd"
-          />
+  function updateGuest(check) {
+    check
+      ? setGuest((x) => x + 1)
+      : guest > 1
+      ? setGuest((x) => x - 1)
+      : setGuest((x) => 1);
+  }
+  return (
+    <div className="">
+      <div className="framer">
+        <div className="android d-flex gx-3 align-items-center">
+          <img src={logo1} className="first-icon" alt={"logo"} />
+          <img src={logo2} className="second-icon" alt={"logo"} />
+        </div>
+        <div className="middle-filter d-flex gx-3 align-items-center ">
+          <div className="select d-flex  align-items-center justify-content-center me-3">
+            <select name="" id="" className=" ">
+              <option value="" selected={true}>
+                Anywhere
+              </option>
+            </select>
+          </div>
+          <div className="d-flex">
+            <div className="d-flex date-icon align-items-center">
+              <img src={calandar} alt={"calendar"} className="calendar" />
+            </div>
+
+            <DatePicker
+              className="me-3"
+              selected={startDate}
+              onChange={onChange}
+              startDate={startDate}
+              endDate={endDate}
+              selectsRange
+              dateFormat="MMM dd"
+            />
+          </div>
+          <div className="d-flex align-items-center btn__guest">
+            <button className="minus__btn" onClick={() => updateGuest(false)}>
+              -
+            </button>
+            <div className="text-input">{guest} guests</div>
+            <button className="plus__btn" onClick={() => updateGuest(true)}>
+              +
+            </button>
+          </div>
+        </div>
+        <div className="filter-menu d-flex align-items-center justify-content-center">
+          <img src={filter} alt={"Filter"} onClick={filterAction} />
         </div>
       </div>
-      <div className="filter-menu"></div>
+      {filterCheck && <Filter close={filterAction} />}
     </div>
   );
 }
---
+++ src/pages/home/MiddleNav/components/Filter/Filter.jsx
@@ -0,0 +1,83 @@
+import "./Filter.css";
+export default function Filter(props) {
+  return (
+    <div className="">
+      <div
+        className="tab-close"
+        onClick={() => {
+          props.close();
+        }}
+      ></div>
+      <div className="filter-page">
+        <div className="d-flex justify-content-between m-5">
+          <div className="filter-text">Filters</div>
+          <div className="clear-all">Clear All</div>
+        </div>
+        <div className="d-flex justify-content-between m-5">
+          <div className="filter-text">Filters</div>
+          <div className="clear-all">Clear All</div>
+        </div>
+        <div className="d-flex justify-content-between m-5">
+          <div className="filter-text">Filters</div>
+          <div className="clear-all">Clear All</div>
+        </div>
+        <div className="d-flex justify-content-between m-5">
+          <div className="filter-text">Filters</div>
+          <div className="clear-all">Clear All</div>
+        </div>
+        <div className="d-flex justify-content-between m-5">
+          <div className="filter-text">Filters</div>
+          <div className="clear-all">Clear All</div>
+        </div>
+        <div className="d-flex justify-content-between m-5">
+          <div className="filter-text">Filters</div>
+          <div className="clear-all">Clear All</div>
+        </div>
+        <div className="d-flex justify-content-between m-5">
+          <div className="filter-text">Filters</div>
+          <div className="clear-all">Clear All</div>
+        </div>
+        <div className="d-flex justify-content-between m-5">
+          <div className="filter-text">Filters</div>
+          <div className="clear-all">Clear All</div>
+        </div>
+        <div className="d-flex justify-content-between m-5">
+          <div className="filter-text">Filters</div>
+          <div className="clear-all">Clear All</div>
+        </div>
+        <div className="d-flex justify-content-between m-5">
+          <div className="filter-text">Filters</div>
+          <div className="clear-all">Clear All</div>
+        </div>
+        <div className="d-flex justify-content-between m-5">
+          <div className="filter-text">Filters</div>
+          <div className="clear-all">Clear All</div>
+        </div>
+        <div className="d-flex justify-content-between m-5">
+          <div className="filter-text">Filters</div>
+          <div className="clear-all">Clear All</div>
+        </div>
+        <div className="d-flex justify-content-between m-5">
+          <div className="filter-text">Filters</div>
+          <div className="clear-all">Clear All</div>
+        </div>
+        <div className="d-flex justify-content-between m-5">
+          <div className="filter-text">Filters</div>
+          <div className="clear-all">Clear All</div>
+        </div>
+        <div className="d-flex justify-content-between m-5">
+          <div className="filter-text">Filters</div>
+          <div className="clear-all">Clear All</div>
+        </div>
+        <div className="d-flex justify-content-between m-5">
+          <div className="filter-text">Filters</div>
+          <div className="clear-all">Clear All</div>
+        </div>
+        <div className="d-flex justify-content-between m-5">
+          <div className="filter-text">Filters</div>
+          <div className="clear-all">Clear All</div>
+        </div>
+      </div>
+    </div>
+  );
+}
